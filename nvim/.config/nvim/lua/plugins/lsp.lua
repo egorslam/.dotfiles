926f89def7b1f20c0b67639845b1585fd47d992d
Yehor Oliinyk <egorslam@users.noreply.github.com>
2026-01-06 20:51:22 -0800

    [nvim] update plugins

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,39 +1,37 @@
-local lsp_zero = require('lsp-zero')
-
+local lsp_zero = require("lsp-zero")
 
 lsp_zero.on_attach(function(_, bufnr)
-  lsp_zero.default_keymaps({ buffer = bufnr })
+	lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
-lsp_zero.setup_servers({ 'ts_ls', 'solargraph', 'lua_ls' })
+lsp_zero.setup_servers({ "ts_ls", "solargraph", "lua_ls" })
 
-
-require('mason').setup({})
-require('mason-lspconfig').setup({
-  ensure_installed = { 'ts_ls', 'solargraph', 'lua_ls' },
-  handlers = {
-    lsp_zero.default_setup,
-  }
+require("mason").setup({})
+require("mason-lspconfig").setup({
+	ensure_installed = { "ts_ls", "solargraph", "lua_ls" },
+	handlers = {
+		lsp_zero.default_setup,
+	},
 })
-local cmp = require('cmp')
+local cmp = require("cmp")
 
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      require("luasnip").lsp_expand(args.body)
-    end,
-  },
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'nvim_lua' },
-    { name = 'buffer' },
-    { name = 'path' },
-  },
-  formatting = lsp_zero.cmp_format(),
-  mapping = cmp.mapping.preset.insert({
-    ['<c-p>'] = cmp.mapping.select_prev_item(),
-    ['<c-n>'] = cmp.mapping.select_next_item(),
-    ['<c-y>'] = cmp.mapping.confirm({ select = true }),
-    ['<c-space>'] = cmp.mapping.complete(),
-  }),
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body)
+		end,
+	},
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "nvim_lua" },
+		{ name = "buffer" },
+		{ name = "path" },
+	},
+	formatting = lsp_zero.cmp_format(),
+	mapping = cmp.mapping.preset.insert({
+		["<c-p>"] = cmp.mapping.select_prev_item(),
+		["<c-n>"] = cmp.mapping.select_next_item(),
+		["<c-y>"] = cmp.mapping.confirm({ select = true }),
+		["<c-space>"] = cmp.mapping.complete(),
+	}),
 })
